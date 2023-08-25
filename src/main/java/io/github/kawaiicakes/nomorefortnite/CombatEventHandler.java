6d5dec862e3c03d4af32dc3ebbfd5f1f47b28a68
kawaiicakes <vomitmopper@gmail.com>
2023-08-25 04:24:14 -0600

    Added check to see if entities already have effect; and if so, to not display text. Also moved the lines to before the effect is added so this actually works lol. Also made particles invisible on L30-31

--- src/main/java/io/github/kawaiicakes/nomorefortnite/CombatEventHandler.java
+++ src/main/java/io/github/kawaiicakes/nomorefortnite/CombatEventHandler.java
@@ -1,5 +1,6 @@
 package io.github.kawaiicakes.nomorefortnite;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -14,18 +15,20 @@ public class CombatEventHandler {
     public static void livingDamageEvent(LivingDamageEvent event) {
         if (!(event.getEntity().level.isClientSide())) {
 
-            //TODO: config
+            //TODO: configurable option to toggle ability to remove inhibited effect (i.e. w/ milk)
             if (event.getSource().getEntity() instanceof ServerPlayer source &&
                     event.getEntity() instanceof ServerPlayer target) {
                 // I hate magic numbers; even trivial ones like this!
                 // Number of ticks per second.
                 final int TPS = 20;
 
-                if (CONFIG.INHIBIT_ATTACKER) source.addEffect(new MobEffectInstance(INHIBITED.get(), CONFIG.INHIBIT_TIMER_ATTACKER * TPS, 1));
-                if (CONFIG.INHIBIT_TARGET) target.addEffect(new MobEffectInstance(INHIBITED.get(), CONFIG.INHIBIT_TIMER_TARGET * TPS, 1));
+                if (CONFIG.NOTIFY_ATTACKER && !source.hasEffect(INHIBITED.get()))
+                    source.sendSystemMessage(Component.translatable("anti_fortnite").withStyle(ChatFormatting.RED), true);
+                if (CONFIG.NOTIFY_TARGET && !target.hasEffect(INHIBITED.get()))
+                    target.sendSystemMessage(Component.translatable("anti_fortnite").withStyle(ChatFormatting.RED), true);
 
-                if (CONFIG.NOTIFY_ATTACKER) source.sendSystemMessage(Component.translatable("anti_fortnite"), true);
-                if (CONFIG.NOTIFY_TARGET) target.sendSystemMessage(Component.translatable("anti_fortnite"), true);
+                if (CONFIG.INHIBIT_ATTACKER) source.addEffect(new MobEffectInstance(INHIBITED.get(), CONFIG.INHIBIT_TIMER_ATTACKER * TPS, 1, false, false, true));
+                if (CONFIG.INHIBIT_TARGET) target.addEffect(new MobEffectInstance(INHIBITED.get(), CONFIG.INHIBIT_TIMER_TARGET * TPS, 1, false, false, true));
             }
         }
     }
